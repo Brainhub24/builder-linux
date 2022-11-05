5ea7eb94a9a7b837015d3944c31e4e23b5ba6245
Peppe289 <gsperanza204@gmail.com>
2022-11-05 12:44:22 +0100

    back
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- build.sh
+++ build.sh
@@ -15,14 +15,3 @@ make -j${CPU_CORE}
 #
 make modules
 make bzImage
-
-
-#
-# Start compression kernel for send via telegram.
-# Remove git history and config (this uses a lot of space).
-#
-rm -rf .git/
-zip -r ../kernel.zip ./*
-
-curl -v -F chat_id="-1001832180909" -F document=@"$(pwd)/../kernel.zip" \
-            https://api.telegram.org/bot"${1}"/sendDocument
