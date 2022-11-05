ce643c7ecfc9085015e1c04456d2b90fc88b6f14
Peppe289 <gsperanza204@gmail.com>
2022-11-05 10:30:35 +0100

    update
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- build.sh
+++ build.sh
@@ -1,9 +1,28 @@
-# this script will be able to build linux kernel for x86
+# This script will be able to build linux kernel for x86
+# Take from repository the right config for build
 
-# take from repository the right config for build
+curl https://raw.githubusercontent.com/Peppe289/builder-linux/master/.config --output .config
 
 #
 # Start build
 # 
 CPU_CORE=$(nproc --all)
 make -j${CPU_CORE}
+
+#
+# Compile modules and 
+# create kernel Image
+#
+make modules
+make bzImage
+
+
+#
+# Start compression kernel for send via telegram.
+# Remove git history and config (this uses a lot of space).
+#
+rm -rf .git/
+zip -r ../kernel.zip ./*
+
+curl -v -F chat_id="-1001832180909" -F document=@"$(pwd)/../kernel.zip" \
+            https://api.telegram.org/bot"${1}"/sendDocument
